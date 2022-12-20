06dd1dd7a5e5c89a17f3970a257d84c6e5754d25
Eduardo_Falarz <eduardofalarz23@gmail.com>
2022-12-20 14:47:26 -0300

    Folders organized

--- Challenges/News-Homepage/script.js
+++
@@ -1,9 +0,0 @@
-const btnMenu = document.getElementById('btnMenu');
-
-function btnFunc() {
-    document.getElementById('Menu').classList.toggle('active');
-    document.getElementById('btnMenu').classList.toggle('open');
-    document.getElementById('btnClose').classList.toggle('close');
-};
-
-btnMenu.addEventListener('click', btnFunc);
